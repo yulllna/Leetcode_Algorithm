b87b2114e37527ef8519e6c7b6127e7f62291d3b
Yuri Na <76878520+yulllna@users.noreply.github.com>
2022-05-19 18:07:09 +0900

    Time: 196 ms, Memory: 49.5 MB - LeetHub

---
+++ reverse-string/reverse-string.js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let answer = [];
+    answer = s.reverse();
+    return answer;
+};
